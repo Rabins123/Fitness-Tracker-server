c265003fda30762c8d2757e2e6e1e65e0c00287e
Rabins Chaudhary <rabinschaudhary76882@gmail.com>
2023-12-22 17:37:00 +0530

    updt path

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,21 @@ const apiRoutes = require("./api");
 
 router.use("/api", apiRoutes);
 
+function base_path(req,res) {
+  return res.json({ a: 1 });
+}
+
+router.use("/test" , base_path);
+
 // connect with react router
 // serve up react front-end in production
-router.use((req, res) => {
-  res.sendFile(path.join(__dirname, "../../build/index.html"));
-});
+
+// router.get('/' , () =>{
+//   return 
+// })
+
+// router.use((req, res) => {
+//   res.sendFile(path.join(__dirname, "../../build/index.html"));
+// });
 
 module.exports = router;
